795b39a37963f0dbd952d9c21bd3bc7f02fe9673
Pedro Henrique de Sousa Varela <p.varela@academico.ifrn.edu.br>
2023-05-31 08:31:29 -0300

    components

--- pos-t01/src/App.tsx
+++ pos-t01/src/App.tsx
@@ -1,7 +1,87 @@
+import React from 'react';
+import './App.css';
+import $ from 'jquery';
+import 'bootstrap/dist/js/bootstrap.bundle';
 
-import './App.css'
+const ToDoItem = (props: any) => {
+  return(
+    <div className="todo-item">
+          <div className="checker">
+            <span><input type="checkbox" /></span>
+         </div>
+        <span>{props.title}</span>
+      <a href="javascript:void(0);" className="float-right remove-todo-item"><i className="icon-close" /></a>
+  </div>
+  )
+}
 
-export const App = () => {
+const MenuItem = (props: any) => {
+  return (
+    <>
+      <li role="presentation" className="nav-item all-task active">
+        <a href="#" className="nav-link">{props.title}</a>
+      </li>
+    </>
+
+  )
+}
+
+export const App: React.FC = () => {
+  React.useEffect(() => {
+    const todo = () => {
+      $(".todo-list .todo-item input").click(function () {
+        if ($(this).is(":checked")) {
+          $(this).parent().parent().parent().toggleClass("complete");
+        } else {
+          $(this).parent().parent().parent().toggleClass("complete");
+        }
+      });
+
+      $(".todo-nav .all-task").click(function () {
+        $(".todo-list").removeClass("only-active");
+        $(".todo-list").removeClass("only-complete");
+        $(".todo-nav li.active").removeClass("active");
+        $(this).addClass("active");
+      });
+
+      $(".todo-nav .active-task").click(function () {
+        $(".todo-list").removeClass("only-complete");
+        $(".todo-list").addClass("only-active");
+        $(".todo-nav li.active").removeClass("active");
+        $(this).addClass("active");
+      });
+
+      $(".todo-nav .completed-task").click(function () {
+        $(".todo-list").removeClass("only-active");
+        $(".todo-list").addClass("only-complete");
+        $(".todo-nav li.active").removeClass("active");
+        $(this).addClass("active");
+      });
+
+      $("#uniform-all-complete input").click(function () {
+        if ($(this).is(":checked")) {
+          $(".todo-item .checker span:not(.checked) input").click();
+        } else {
+          $(".todo-item .checker span.checked input").click();
+        }
+      });
+
+      $(".remove-todo-item").click(function () {
+        $(this).parent().remove();
+      });
+    };
+
+    todo();
+  }, []);
+
+  const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter' && e.currentTarget.value.length > 0) {
+      $('<div class="todo-item"><div class="checker"><span class=""><input type="checkbox"></span></div> <span>' + e.currentTarget.value + '</span> <a href="javascript:void(0);" class="float-right remove-todo-item"><i class="icon-close"></i></a></div>').insertAfter('.todo-list .todo-item:last-child');
+      e.currentTarget.value = "";
+    } else if (e.key === 'Enter') {
+      alert("Please enter a new task");
+    }
+  };
 
   return (
     <>
@@ -15,83 +95,26 @@ export const App = () => {
                     type="text"
                     className="form-control add-task"
                     placeholder="New Task..."
+                    onKeyPress={handleKeyPress}
                   />
                 </form>
                 <ul className="nav nav-pills todo-nav">
-                  <li
-                    role="presentation"
-                    className="nav-item all-task active"
-                  >
-                    <a href="#" className="nav-link">All</a>
-                  </li>
-                  <li
-                    role="presentation"
-                    className="nav-item active-task"
-                  >
-                    <a href="#" className="nav-link">Active</a>
-                  </li>
-                  <li
-                    role="presentation"
-                    className="nav-item completed-task"
-                  >
-                    <a href="#" className="nav-link">Completed</a>
-                  </li>
+                  <MenuItem title="All" />
+                  <MenuItem title="Active" />
+                  <MenuItem title="Completed" />
                 </ul>
                 <div className="todo-list">
-                  <div className="todo-item">
-                    <div className="checker">
-                      <span 
-                        ><input type="checkbox"
-                      /></span>
-                    </div>
-                    <span>Create theme</span>
-                    <a
-                      href="javascript:void(0);"
-                      className="float-right remove-todo-item"
-                      ><i className="icon-close"/></a>
-                  </div>
-                  <div className="todo-item">
-                    <div className="checker">
-                      <span 
-                        ><input type="checkbox"
-                      /></span>
-                    </div>
-                    <span>Work on wordpress</span>
-                    <a
-                      href="javascript:void(0);"
-                      className="float-right remove-todo-item"
-                      ><i className="icon-close"></i></a>
-                  </div>
-                  <div className="todo-item">
-                    <div className="checker">
-                      <span 
-                        ><input type="checkbox"
-                      /></span>
-                    </div>
-                    <span>Organize office main department</span>
-                    <a
-                      href="javascript:void(0);"
-                      className="float-right remove-todo-item"
-                      ><i className="icon-close"></i></a>
-                  </div>
-                  <div className="todo-item">
-                    <div className="checker">
-                      <span><input type="checkbox" /></span>
-                    </div>
-                    <span>Error solve in HTML template</span>
-                    <a
-                      href="javascript:void(0);"
-                      className="float-right remove-todo-item"
-                      ><i className="icon-close"></i></a>
-                  </div>
-                </div>
+                  <ToDoItem title="Create Theme"/>
+                  <ToDoItem title="Create Theme"/>
               </div>
             </div>
           </div>
         </div>
       </div>
-      
-    </>
-  )
-}
+    </div>
+  </>
+  );
+};
+
+export default App;
 
